398bfc7f08898f0dc2951339cc9400222db7ce96
Bronty <at3177198@gmail.com>
2024-09-17 05:01:04 +0900

    Change:DetailViewのデザイン改善,動的テキスト生成

--- src/js/detailView.js
+++ src/js/detailView.js
@@ -1,4 +1,10 @@
 const gameDetailView = {
+    props:[
+        'title',
+        'description',
+        'thumbnailPath',
+        'imgsrc'
+    ],
     methods:{
         closeDetailView(){
             console.log('close detail view');
@@ -9,16 +15,20 @@ const gameDetailView = {
                 this.closeDetailView();
             }
         }
-    }
-    ,
+    },
     template:`
     <div class="game-detail-overlay" @click="clickOverlay($event)">
         <div class="game-detail">
-            <img class="game-detail-img" src="img/NumHuntMenuScreen.png">
+            <div class="close-detail-button" @click="closeDetailView()">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="" class="bi bi-x" viewBox="0 0 16 16">
+                <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/>
+                </svg>
+            </div>
+            <img class="game-detail-img" :src="thumbnailPath">
             <div class="game-description-container">
-                <h3 class="game-detail-title">NumHunt</h3>
+                <h3 class="game-detail-title">{{title}}</h3>
                 <p class="game-detail-description">
-                    NumHuntは、数字を使ったオンラインカードゲーム。最大4人で対戦できるこのゲームは、プレイヤーは、数字を駆使し「ボーナスナンバー」の獲得を目指す。
+                    {{description}}
                 </p>
                 <div class="game-spec-table">
                     <div class="game-spec-table-row">
